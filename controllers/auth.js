ab31b397b37211fd9e57e8a2f2e064c81c1964b2
Zaur Dadashov <zaur.dadashov@gmail.com>
2023-05-05 11:15:54 +0400

    Feat:Parsing user data

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,10 @@
-module.exports.login = function (req, res) {};
+module.exports.login = function (req, res) {
+  res.status(200).json({
+    login: {
+      email: req.body.email,
+      password: req.body.password,
+    },
+  });
+};
 
 module.exports.register = function (req, res) {};
