5dca09866ff927e70c6bf88c1771d8d2c5bbd76e
Zaur Dadashov <zaur.dadashov@gmail.com>
2023-05-10 11:42:55 +0400

    Feat:Category functionality and file upload configuration

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const passport = require("passport")
+const passport = require("passport");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
 const analyticsRoutes = require("./routes/analytics");
 const categoryRoutes = require("./routes/category");
 const orderRoutes = require("./routes/order");
 const positionRoutes = require("./routes/position");
-const keys = require ('./config/keys')
+const keys = require("./config/keys");
 const app = express();
 
 mongoose
@@ -15,10 +15,11 @@ mongoose
   .then(() => console.log("MongoDb connected"))
   .catch((error) => console.log(error));
 
-app.use(passport.initialize())
-require('./middleware/passport')(passport)
+app.use(passport.initialize());
+require("./middleware/passport")(passport);
 
 app.use(require("morgan")("dev"));
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require("cors")());
